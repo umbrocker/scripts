3b4d481169055206bb490a16deee28fea85e5ff9
Umi <umbrocker@gmail.com>
2025-01-11 22:10:44 +0100

    modified settarget.sh and it.sh

--- it.sh
+++ it.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo -n "python -c 'import pty;pty.spawn(\"/bin/bash\")'"
+	echo -en "[*] Copied to clipboard:\npython -c 'import pty;pty.spawn(\"/bin/bash\")'"
 	echo -n "python -c 'import pty;pty.spawn(\"/bin/bash\")'" | xclip -sel clip
 elif [ "$1" == "3" ]; then
-	echo -n "python3 -c 'import pty;pty.spawn(\"/bin/bash\")'"
+	echo -en "[*] Copied to clipboard:\npython3 -c 'import pty;pty.spawn(\"/bin/bash\")'"
 	echo -n "python3 -c 'import pty;pty.spawn(\"/bin/bash\")'" | xclip -sel clip
 else
-    echo "Nem értelmezhető"
+    echo "[*] Usage:"
+	echo "[*] For normal python install: $0"
+	echo "[*] For python3 install: $0 3"
 fi
 
--- settarget.sh
+++ settarget.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
-# usage:
-# chmod +x settarget
-# source ./settarget <target_ip>
 if [ $# -ne 1 ]; then
     echo "[*] Usage: $0 <target_ip>"
-    read
     exit 1
 fi
 
 
 new_ip=$1
-echo $new_ip > /home/$(whoami)/target
-export target=$new_ip
+directory="$HOME/.config"
+
+if [ -d "$directory" ]; then
+	:
+else
+  mkdir -p $directory
+fi
+echo $new_ip > $directory/target 
+
+current_shell=$(basename "$SHELL")
+
+# Meghatározza az rc fájl nevét a shell alapján
+case "$current_shell" in
+  bash)
+    rc_file="$HOME/.bashrc"
+    ;;
+  zsh)
+    rc_file="$HOME/.zshrc"
+    ;;
+  fish)
+    rc_file="$HOME/.config/fish/config.fish"
+    ;;
+  *)
+    echo "[!] Unexpected shell type: $current_shell"
+    exit 1
+    ;;
+esac
+
+export_line="export target=$(cat $directory/target)"
+
+if grep -q "^export target=" "$rc_file"; then
+	sed -i "s/^export target=.*/$export_line/" "$rc_file"
+	echo "[*] Updated: '$export_line' in $rc_file file."
+else
+  	echo "$export_line" >> "$rc_file"
+  	echo "[+] Added: '$export_line' to $rc_file file."
+fi
+echo "[!] You need to use this command: source $rc_file"
+
 hostname="target"
 hosts_file="/etc/hosts"
 
@@ -23,5 +56,4 @@ if grep -q "\s$hostname$" "$hosts_file"; then
 else
     echo -e "$new_ip\t$hostname" | sudo tee -a "$hosts_file"
 fi
-
-cat /etc/hosts
+echo -e "[*] last line of /etc/hosts file:\n$(grep target $hosts_file)"
