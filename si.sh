087fac0c6d337fa82629afbbc0a2fdc2a4fa82e5
Gipsz Jakab <jakab@gipsz.com>
2025-02-15 20:03:23 +0100

    replaced si.sh

--- si.sh
+++ si.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 # Description:
-# 	Simple network interface lister.
-# Dependecies:
-# 	ip,awk,cut
+#   Simple network interface lister.
+# Dependencies:
+#   ip, awk, cut, column
 # Short description: si | showing all interfaces with IP addresses
 
-echo -e "┌───────────────────────┬───────────────────────┐"
-echo -e "│\tInterface\t│\tAddress\t\t│"
-echo -e "├───────────────────────┼───────────────────────┤"
+
+# Gyűjtsük össze az adatokat egy változóba
+output=""
 for interface in $(ip -o link show | awk -F': ' '{print $2}'); do
     ip_address=$(ip -4 -o addr show $interface | awk '{print $4}' | cut -d'/' -f1)
     if [ -n "$ip_address" ]; then
-        string_length=${#interface}
-	if [ $string_length -gt 4 ]; then
-	    echo -e "│\t$interface\t│\t$ip_address\t│"
-	else
-	    echo -e "│\t$interface\t\t│\t$ip_address\t│"
-	fi
+        output+="│$interface\t$ip_address \n"
     fi
 done
-echo -e "└───────────────────────┴───────────────────────┘"
+
+# Formázott táblázat kiírása
+echo -e "$output" | column -t -s $'\t' -N '│INTERFACE',ADDRESS -o │
+
