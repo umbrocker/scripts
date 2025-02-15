195e01aff8c425a7a4872a2c5ad1c83f0200a1ed
Gipsz Jakab <Gipsz@Jakab.com>
2025-02-15 20:05:51 +0100

    modified si.sh

--- si.sh
+++ si.sh
@@ -12,10 +12,10 @@ output=""
 for interface in $(ip -o link show | awk -F': ' '{print $2}'); do
     ip_address=$(ip -4 -o addr show $interface | awk '{print $4}' | cut -d'/' -f1)
     if [ -n "$ip_address" ]; then
-        output+="│$interface\t$ip_address \n"
+        output+="│ $interface \t $ip_address \n"
     fi
 done
 
 # Formázott táblázat kiírása
-echo -e "$output" | column -t -s $'\t' -N '│INTERFACE',ADDRESS -o │
+echo -e "$output" | column -t -s $'\t' -N '│ INTERFACE ',' ADDRESS ' -o │
 
